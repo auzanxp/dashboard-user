082cfb80031bd0ec8285d25d23632b57e125f00e
engineering <istikhoriauzan@gmail.com>
2023-01-16 18:09:35 +0700

    update button

--- src/components/dashboard/Sidebar.js
+++ src/components/dashboard/Sidebar.js
@@ -13,7 +13,7 @@ export default function Sidebar() {
     return (
         <>
             <div className="mb-4 pb-2 border-b-2 border-b-gray-300 py-6 px-5">
-                <Brand />
+                <Brand isSmall />
             </div>
             <div className="border-b">
                 <SideNavLink
--- src/elements/Button.js
+++ src/elements/Button.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Button({ children, color, ...props }) {
-    const classes = [
+    const clasName = [
         'flex items-center tracking-wider justify-center text-sm sm:text-md text-white rounded-md transition duration-200',
     ];
 
-    if (!color) classes.push(' bg-indigo-400 hover:bg-indigo-500');
+    if (!color) clasName.push(' bg-indigo-400 hover:bg-indigo-500');
 
-    if (color === 'yellow') classes.push(' bg-amber-400 hover:bg-amber-500');
+    if (color === 'yellow') clasName.push(' bg-amber-400 hover:bg-amber-500');
 
-    if (color === 'red') classes.push(' bg-red-600  hover:bg-red-700');
+    if (color === 'red') clasName.push(' bg-red-600  hover:bg-red-700');
 
-    props.isLarge ? classes.push(' py-2 px-4') : classes.push(' py-1 px-3');
+    props.isLarge ? clasName.push(' py-2 px-4') : clasName.push(' py-1 px-3');
 
     if (props.type === 'link') {
         return (
-            <Link to={props.href} className={`${classes.join('')} w-fit gap-x-2 [&>svg]:w-5 [&>svg]:h-5`}>
+            <Link to={props.href} className={`${clasName.join('')} w-fit gap-x-2 [&>svg]:w-5 [&>svg]:h-5`}>
                 {children}
             </Link>
         );
@@ -25,7 +25,7 @@ export default function Button({ children, color, ...props }) {
             <button
                 onClick={props.onClick}
                 type={props.type}
-                className={`${classes.join('')} gap-x-2 [&>svg]:w-5 [&>svg]:h-5`}
+                className={`${clasName.join('')} gap-x-2 [&>svg]:w-5 [&>svg]:h-5`}
             >
                 {children}
             </button>
