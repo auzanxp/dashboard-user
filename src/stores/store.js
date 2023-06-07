f6918f30af06cbf1d7871e7d5cb975b3a706f749
Auzan <istikhoriauzan@gmail.com>
2023-06-07 23:41:18 +0700

    feat: create store

---
+++ src/stores/store.js
@@ -0,0 +1,15 @@
+import { create } from 'zustand'
+
+export const useAppStore = create((set) => ({
+    count: 0,
+    increase: () => {
+        set((state) => ({
+            count: state.count + 1,
+        }))
+    },
+    decrease: () => {
+        set((state) => ({
+            count: state.count -1,
+        }))
+    }
+}))
